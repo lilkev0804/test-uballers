eb9e24d5f7f37669ef67a0ee23e55e7424696f51
lilkev0804 <56228273+lilkev0804@users.noreply.github.com>
2021-06-25 13:38:32 +0200

    card create and props r ready, now i need to add star icon for favorite

--- src/components/cartPlayground/card.js
+++ src/components/cartPlayground/card.js
@@ -4,30 +4,39 @@ import {
   StyleSheet,
   View,
   Text,
+  Button,
   TouchableOpacity,
 } from 'react-native';
 
 const image = {
   uri: 'https://images.unsplash.com/photo-1579158950237-a1d86ef408c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80',
 };
-export default function Card(props) {
+export default function Card({navigation, title}) {
   return (
     <View style={style.containerCard}>
-      <ImageBackground source={image} style={style.image}>
-        
-      </ImageBackground>
-      <TouchableOpacity style={style.button} />
+      <ImageBackground
+        imageStyle={{borderRadius: 25}}
+        source={image}
+        style={style.image}></ImageBackground>
+
+      <TouchableOpacity
+        style={style.button}
+        onPress={() => navigation.navigate('PlayGround')}>
+        <Text style={style.name}>{title}</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const style = StyleSheet.create({
   containerCard: {
+    margin: 20,
+    padding: 15,
     flex: 1,
-    justifyContent: 'space-evenly',
     flexDirection: 'column',
-    height: 200,
-    width: 400,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    minHeight: 250,
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
@@ -36,17 +45,28 @@ const style = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
-    padding: 50,
+    borderRadius: 25,
+    backgroundColor: 'white',
   },
+
   image: {
     flex: 1,
-    height: 200,
+    height: 150,
+    width: 300,
     resizeMode: 'cover',
     justifyContent: 'center',
+    marginBottom: 15,
   },
   button: {
     alignItems: 'center',
-    backgroundColor: '#484848',
     padding: 10,
+    width: 300,
+    borderRadius: 50,
+    backgroundColor: '#DC691D',
+  },
+  name: {
+    color: 'white',
+    fontSize: 18,
+    textAlign: 'center',
   },
 });
--- src/screens/playgroundList.js
+++ src/screens/playgroundList.js
@@ -1,28 +1,21 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {SafeArray, Text, StyleSheet, ScrollView} from 'react-native';
 import data from '../groundsData.json';
-import axios from 'axios';
+
 import Card from '../components/cartPlayground/Card';
 
 export default function PlaygroundList() {
   return (
-    <View style={style.containerAllGround}>
-      {/* {data.allGrounds.map((ground, i) => ( */}
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      {/* ))} */}
-    </View>
+    <ScrollView style={style.containerAllGround}>
+      {data.allGrounds.map((ground, i) => (
+        <Card key={i} title={ground.groundName}></Card>
+      ))}
+    </ScrollView>
   );
 }
 
 const style = StyleSheet.create({
   containerAllGround: {
     flex: 1,
-    width: 400,
-    alignContent: 'center',
-    flexDirection: 'column',
-    justifyContent: 'center',
-
   },
 });
